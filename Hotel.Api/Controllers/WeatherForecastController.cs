dd3d0d6ae7de1664b5776e0ff41774f2d9aa9d09
HassanSoliman <solimanh645@gmail.com>
2025-09-24 00:19:02 +0300

    commit

--- Hotel.Api/Controllers/WeatherForecastController.cs
+++ Hotel.Api/Controllers/WeatherForecastController.cs
@@ -27,7 +27,7 @@ namespace Hotel.Api.Controllers
                 TemperatureC = Random.Shared.Next(-20, 55),
                 Summary = Summaries[Random.Shared.Next(Summaries.Length)]
             })
-            .ToArray();
+            .ToList();
         }
     }
 }
