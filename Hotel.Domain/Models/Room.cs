291d6dfd9769c325f523e36ab4af87272529906e
Ahmed El-Srogy <srogy16@gmail.com>
2025-09-24 19:38:58 +0300

    Create room

---
+++ Hotel.Domain/Models/Room.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hotel.Domain.Models
+{
+    public class Room
+    {
+        public Guid Id { get; set; } 
+        public string RoomNumber { get; set; } = string.Empty;
+        public string Type { get; set; } = string.Empty;
+        public decimal PricePerNight { get; set; }
+        public bool IsAvailable { get; set; } = true;
+        public string? Description { get; set; }
+    }
+}
